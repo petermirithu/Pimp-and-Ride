0948027ccc616c73bf79dc5f954c3671fb542c3e
petermirithu <pyra_m.k@yahoo.com>
2020-05-29 22:01:56 +0300

    add routes to display sign up and sign in page

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/signup', function(req, res) {
+  res.render('signup', { title: 'Sign Up' });
 });
-
+router.get('/signin', function(req, res) {
+  res.render('signin', { title: 'Sign In' });
+});
 module.exports = router;
