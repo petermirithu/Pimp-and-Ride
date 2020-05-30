3dd090f41b24e5a358c0443c0b422d4fdb09afad
petermirithu <pyra_m.k@yahoo.com>
2020-05-30 20:28:23 +0300

    add products to home page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const Product = require("../models").products;
+const Category = require("../models").category;
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'BetMac'});
+router.get('/', async(req, res) => {  
+  let products = await Product.findAll({include: [{model: Category,as: 'Category',}]})
+  res.render('index', { title: 'BetMac',products:products});
 });
+
 router.get('/about', function(req, res) {
   res.render('about', { title: 'About'});
 });
