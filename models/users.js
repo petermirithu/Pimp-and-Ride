71c4ec72833e50e118aac159f73b819245b1c74a
petermirithu <pyra_m.k@yahoo.com>
2020-06-01 19:43:11 +0300

    add admin priviledges

--- models/users.js
+++ models/users.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const users = sequelize.define('users', {
     username: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    admin: DataTypes.BOOLEAN
   }, {});
   users.associate = function(models) {
     // associations can be defined here
