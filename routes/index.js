09d9044688e6d3bbc2772ac1b033429f50b39aad
petermirithu <pyra_m.k@yahoo.com>
2020-06-26 18:12:23 +0300

    add filter for items by category and new routes

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,18 @@ router.get('/', async(req, res) => {
   res.render('index---2', { title: 'Pimp & Ride',products:products,cart:cart});
 });
 
+router.get('/products/:category', async(req, res) => {  
+  category_x= await Category.findOne({ where:{name:req.params.category}})  
+  let products = await Product.findAll({ where:{categoryId:category_x.id},include: [{model: Category,as: 'Category',}]})  
+  var cart=null
+  if (req.user){
+    cart = await Cart.findOne(
+      {include: [{model: Order,as: 'Cart',include: [{model:Product,as:'Product'}]}],
+      where:{userId:req.user.id,ordered:false}})        
+  }
+  res.render('ourproducts', { title: category_x.name ,products:products,cart:cart});
+});
+
 router.get('/about', async(req, res) => {
   var cart=null
   if (req.user){
@@ -24,17 +36,17 @@ router.get('/about', async(req, res) => {
       {include: [{model: Order,as: 'Cart',include: [{model:Product,as:'Product'}]}],
       where:{userId:req.user.id,ordered:false}})        
   }
-  res.render('about', { title: 'About',cart:cart});
+  res.render('about---2', { title: 'About Us',cart:cart});
 });
 
-router.get('/services', async(req, res) => {
+router.get('/contact', async(req, res) => {
   var cart=null
   if (req.user){
     cart = await Cart.findOne(
       {include: [{model: Order,as: 'Cart',include: [{model:Product,as:'Product'}]}],
       where:{userId:req.user.id,ordered:false}})        
   }
-  res.render('services', { title: 'Services',cart:cart});
+  res.render('contact', { title: 'Contact Us',cart:cart});
 });
 
 module.exports = router;
